5b4c2c213fde623a71217c7996819c2697d05564
WebTheoryMedia-Steve <58831368+WebTheoryMedia-Steve@users.noreply.github.com>
2024-10-22 18:05:12 -0400

    Fixing RegEx

--- elementor-phone-autoformat.php
+++ elementor-phone-autoformat.php
@@ -10,7 +10,7 @@ Author: WebTheory Media LLC
 function epw_enqueue_phone_auto_format_script() {
     // Only load the script on pages with Elementor forms
     if ( ! is_admin() && has_shortcode( get_post()->post_content, 'elementor' ) ) {
-        wp_enqueue_script( 'epw-phone-auto-format', plugin_dir_url( __FILE__ ) . 'phone-auto-format.js', array(), '1.0', true );
+        wp_enqueue_script( 'epw-phone-auto-format', plugin_dir_url( __FILE__ ) . 'phone-auto-format.js', array(), '1.1', true );
     }
 }
 add_action( 'wp_enqueue_scripts', 'epw_enqueue_phone_auto_format_script' );
